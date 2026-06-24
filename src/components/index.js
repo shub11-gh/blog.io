6a58eb8f3ed9a0aa38f7319a061e0819c522e9f3
Shubham <shubhamjangir1117@gmail.com>
2026-06-24 23:01:05 +0530

    add RTE, post form

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import Logo from "./Logo/Logo"
 import LogoutButton from "./Header/LogoutButton"
-export { Header, Footer, Container, Logo, LogoutButton }
+import RTE from "./RTE/RTE"
+export { Header, Footer, Container, Logo, LogoutButton, RTE }
